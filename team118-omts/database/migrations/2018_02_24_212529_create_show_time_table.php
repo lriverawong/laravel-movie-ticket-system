a35aa4d3144b5aeccfdad26fe601ab55f3258ee6
Luis Rivera-Wong <lriverawong@outlook.com>
2018-02-24 18:05:27 -0500

    created reservation table

--- team118-omts/database/migrations/2018_02_24_212529_create_show_time_table.php
+++ team118-omts/database/migrations/2018_02_24_212529_create_show_time_table.php
@@ -14,6 +14,7 @@ class CreateShowTimeTable extends Migration
     public function up()
     {
         Schema::create('show_time', function (Blueprint $table) {
+            $table->increments('id');
             $table->integer('movie_id')->unsigned();
             $table->foreign('movie_id')->references('id')->on('movies')->onDelete('cascade');
             $table->integer('theatre_id')->unsigned();
@@ -21,7 +22,7 @@ class CreateShowTimeTable extends Migration
             $table->integer('theatre_complex_id')->unsigned();
             $table->foreign('theatre_complex_id')->references('id')->on('theatre_complexes')->onDelete('cascade');
             $table->dateTime('showing_start_time');
-            $table->primary(['movie_id', 'theatre_id', 'theatre_complex_id', 'showing_start_time'], 'showing_id');
+            $table->unique(['movie_id', 'theatre_id', 'theatre_complex_id', 'showing_start_time'], 'showing_id');
             $table->unsignedInteger('num_seats_avail');
             $table->timestamps();
         });
---
+++ team118-omts/database/migrations/2018_02_24_225510_create_reservation_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReservationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservation', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('showing_id');
+            $table->foreign('showing_id')->references('id')->on('show_time');
+            $table->integer('number_of_tickets')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservation');
+    }
+}
