a18e3756b5e0d8782f7f3dc50c7e7a78a01fb8ce
Luis Rivera-Wong <lriverawong@outlook.com>
2018-03-23 07:44:58 -0400

    added basic controller functions for theatre-complex

---
+++ team118-omts/resources/assets/js/API/TheatreComplexesController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Request;
+
+class TheatreComplexesController extends Controller {
+    // index
+    public function getComplexes() {
+
+    }
+    // show
+    public function getComplex($id) {
+
+    }
+    /** Create
+     * this method using an implicit binding to the Request object. 
+     * This will allow us to grab the form data sent from the user to add a new cafe.
+     */
+    public function postComplex() {
+
+    }
+}
