2002df63d099383862c84f268d4d60419ac3de24
Luis Rivera-Wong <lriverawong@outlook.com>
2018-03-29 22:27:25 -0400

    view control based on admin priv

--- team118-omts/resources/views/basehome.blade.php
+++ team118-omts/resources/views/basehome.blade.php
@@ -1,3 +1,12 @@
 <div class="container">
-    
+    <p>{{ auth()->user()->role }}</p>
+    @if (auth()->user()->isAdmin())
+        <div>
+            HERE
+        </div>
+    @endif
+    <div>
+        not here
+    </div>
+
 </div>
