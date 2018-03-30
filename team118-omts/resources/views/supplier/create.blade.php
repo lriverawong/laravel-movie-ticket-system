68a5f4e52c865acb633d673ec1a6a68411d48d0e
Quentin Petraroia <q.petraroia@gmail.com>
2018-03-30 19:31:55 -0400

    finished admin panel

--- team118-omts/resources/views/supplier/create.blade.php
+++ team118-omts/resources/views/supplier/create.blade.php
@@ -1,102 +1,105 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        @include ('supplier.list')
-        <div id="supplier-form" class="container">
-            <form method="POST" action="/suppliers" @submit.prevent="onSubmit" @keydown="form.errors.clear($event.target.name)">
-                <div class="form-group">
+<div class="container">
+        <div class="container">
+            <form method="POST" action="{{URL('suppliers')}}">
+            @csrf
+                <div class="row">
+                    <div class="col-md-4"></div>
+                    <div class="form-group col-md-4">
                     <label for="name" class="label">Supplier Name:</label>
                     
-                    <input type="text" id="name" name="name" class="input" v-model="form.name"> 
+                    <input type="text" id="name" name="name" class="form-control"> 
 
-                    <span class="form-text has-danger" v-if="form.errors.has('name')" v-text="form.errors.get('name')"></span>
+                    <span class="form-text has-danger"></span>
                 </div>
     
                 <div class="form-group">
-                    <label for="phone_num" class="label">Supplier Phone Num:</label>
+                    <label for="phone_num" class="label">Supplier Phone Number:</label>
                     
-                    <input type="text" id="phone_num" name="phone_num" class="input" v-model="form.phone_num">
+                    <input type="text" id="phone_num" name="phone_num" class="form-control">
 
-                    <span class="form-text has-danger" v-if="form.errors.has('phone_num')" v-text="form.errors.get('phone_num')"></span>
+                    <span class="form-text has-danger"></span>
                 </div>
 
                 <div class="form-group">
-                    <label for="contact_first_name" class="label">Supplier Contact First Name</label>
-                    
-                    <input type="text" id="contact_first_name" name="contact_first_name" class="input" v-model="form.contact_first_name">
+                    <label for="contact_first_name" class="label">Supplier Contact Name:</label>
+                        
+                    <input type="text" id="contact_first_name" name="contact_first_name" class="form-control">
     
-                    <span class="form-text has-danger" v-if="form.errors.has('contact_first_name')" v-text="form.errors.get('contact_first_name')"></span>
+                    <span class="form-text has-danger"></span>
                 </div>
 
                 <div class="form-group">
-                    <label for="contact_last_name" class="label">Supplier Contact Last Name</label>
-    
-                    <input type="text" id="contact_last_name" name="contact_last_name" class="input" v-model="form.contact_last_name">
-    
-                    <span class="form-text has-danger" v-if="form.errors.has('contact_last_name')" v-text="form.errors.get('contact_last_name')"></span>
-                </div>
-                
-                <div class="form-group">
-                        <label for="apt_num" class="label">Supplier Apartment Number</label>
-    
-                        <input type="text" id="apt_num" name="apt_num" class="input" v-model="form.apt_num">
+                    <label for="contact_last_name" class="label">Supplier Contact Name:</label>
+                            
+                    <input type="text" id="contact_last_name" name="contact_last_name" class="form-control">
         
-                        <span class="form-text has-danger" v-if="form.errors.has('apt_num')" v-text="form.errors.get('apt_num')"></span>
-                    </div>
-
-                <div class="control">
-                    <label for="street_num" class="label">Supplier Street Num:</label>
-                    
-                    <input type="text" id="street_num" name="street_num" class="input" v-model="form.street_num">
-
-                    <span class="help is-danger" v-if="form.errors.has('street_num')" v-text="form.errors.get('street_num')"></span>
+                    <span class="form-text has-danger"></span>
                 </div>
 
-                <div class="control">
-                    <label for="street_name" class="label">Supplier Street Name:</label>
-                    
-                    <input type="text" id="street_name" name="street_name" class="input" v-model="form.street_name">
-
-                    <span class="help is-danger" v-if="form.errors.has('street_name')" v-text="form.errors.get('street_name')"></span>
-                </div>
-                
-
-                <div class="control">
-                    <label for="city" class="label">Supplier Street City:</label>
-                    
-                    <input type="text" id="city" name="city" class="input" v-model="form.city">
-
-                    <span class="help is-danger" v-if="form.errors.has('city')" v-text="form.errors.get('city')"></span>
-                </div>
-
-                <div class="control">
-                    <label for="province" class="label">Supplier Province:</label>
-                
-                    <input type="text" id="province" name="province" class="input" v-model="form.province">
-
-                    <span class="help is-danger" v-if="form.errors.has('province')" v-text="form.errors.get('province')"></span>
-                </div>
-
-                <div class="control">
-                    <label for="country" class="label">Supplier Country:</label>
-                
-                    <input type="text" id="country" name="country" class="input" v-model="form.country">
-
-                    <span class="help is-danger" v-if="form.errors.has('country')" v-text="form.errors.get('country')"></span>
-                </div>
-
-                <div class="control">
-                    <label for="postal_code" class="label">Supplier Postal Code:</label>
-                
-                    <input type="text" id="postal_code" name="postal_code" class="input" v-model="form.postal_code">
-
-                    <span class="help is-danger" v-if="form.errors.has('postal_code')" v-text="form.errors.get('postal_code')"></span>
-                </div>
-                
-            
                 <div class="form-group">
-                    <button class="btn btn-primary">Create</button>
+                    <label for="apt_num" class="label">Supplier Apartment Number:</label>
+                                
+                    <input type="text" id="apt_num" name="apt_num" class="form-control">
+            
+                    <span class="form-text has-danger"></span>
+                </div>
+
+                <div class="form-group">
+                    <label for="street_num" class="label">Supplier Street Number:</label>
+                                    
+                    <input type="text" id="street_num" name="street_num" class="form-control">
+                
+                    <span class="form-text has-danger"></span>
+                </div>
+
+                <div class="form-group">
+                    <label for="street_name" class="label">Supplier Street Name:</label>
+                                        
+                    <input type="text" id="street_name" name="street_name" class="form-control">
+                    
+                    <span class="form-text has-danger"></span>
+                </div>
+
+                <div class="form-group">
+                    <label for="city" class="label">Supplier City:</label>
+                                        
+                    <input type="text" id="city" name="city" class="form-control">
+                    
+                    <span class="form-text has-danger"></span>
+                </div>
+
+                <div class="form-group">
+                    <label for="province" class="label">Supplier Province:</label>
+                                            
+                    <input type="text" id="province" name="province" class="form-control">
+                        
+                    <span class="form-text has-danger"></span>
+                </div>
+
+                <div class="form-group">
+                    <label for="country" class="label">Supplier Country:</label>
+                                            
+                    <input type="text" id="country" name="country" class="form-control">
+                        
+                    <span class="form-text has-danger"></span>
+                </div>
+
+                <div class="form-group">
+                    <label for="postal_code" class="label">Supplier Postal Code:</label>
+                                                
+                    <input type="text" id="postal_code" name="postal_code" class="form-control">
+                            
+                    <span class="form-text has-danger"></span>
+                </div>
+                
+                <div class="row">
+                    <div class="col-md-4"></div>
+                    <div class="form-group col-md-4" style="margin-top:60px">
+                        <button type="submit" class="btn btn-success" style="margin-left:38px">Create</button>
+                    </div>
                 </div>
             </form>
         </div>
---
+++ team118-omts/resources/views/supplier/index.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Inside index page of suppliers</h1>
+        @if (count($suppliers))
+            <h1 class="title is-3">Supplier</h1>
+        @endif
+        <table class="table">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">Supplier Name</th>
+                    <th scope="col">Supplier Phone Number</th>
+                    <th scope="col">Supplier Contact First Name</th>
+                    <th scope="col">Supplier Contact Last Name</th>
+                    <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($suppliers as $supplier)
+                <tr>
+                    <th scope="row">{{$supplier->name}}</th>
+                    <td>{{$supplier->phone_num}}</td>
+                    <td>{{$supplier->contact_first_name}}</td>
+                    <td>{{$supplier->contact_last_name}}</td>
+                    <td>
+                        <div class="btn btn-group" role="group" aria-label="Basic example">
+                            <a href="{{ URL::to('suppliers/' . $supplier->id . '/edit') }}">
+                            <button type="button" class="btn btn-warning">Edit</button>
+                            </a>&nbsp;
+                            <form action="{{url('suppliers', [$supplier->id])}}" method="POST">
+                                <input type="hidden" name="_method" value="DELETE">
+                                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                <input type="submit" class="btn btn-danger" value="Delete"/>
+                            </form>
+                        </div>
+                    </td>
+                </tr>
+                    {{--  <li>
+                        <a href="#">{{ $director->last_name }}</a>
+                    </li>  --}}
+                @endforeach
+            
+
+            <hr>
+    </div>
+@endsection
