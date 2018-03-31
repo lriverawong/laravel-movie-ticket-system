c151f54decd6393cbd51bcfdb4dd1bb357602a8c
Quentin Petraroia <q.petraroia@gmail.com>
2018-03-31 01:29:13 -0400

    finished movies admin panel

--- team118-omts/resources/views/movie/create.blade.php
+++ team118-omts/resources/views/movie/create.blade.php
@@ -1,71 +1,96 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        @include ('movie.list')
-        <div id="movie-form" class="container">
-            <form method="POST" action="/movies" @submit.prevent="onSubmit" @keydown="form.errors.clear($event.target.name)">
-                <div class="form-group">
-                    <label for="title" class="label">Movie title:</label>
+<div class="container">
+        <div class="container">
+            <form method="POST" action="{{URL('movies')}}">
+            @csrf
+                <div class="row">
+                    <div class="col-md-4"></div>
+                    <div class="form-group col-md-4">
+                    <label for="title" class="label">Movie Title:</label>
                     
-                    <input type="text" id="title" name="title" class="input" v-model="form.title"> 
+                    <input type="text" id="title" name="title" class="form-control" value="{{old('title')}}"> 
 
-                    <span class="form-text has-danger" v-if="form.errors.has('title')" v-text="form.errors.get('title')"></span>
+                    <span class="form-text has-danger"></span>
                 </div>
     
                 <div class="form-group">
                     <label for="running_time" class="label">Movie Running Time:</label>
                     
-                    <input type="number" id="running_time" name="running_time" class="input" v-model="form.running_time">
+                    <input type="text" id="running_time" name="running_time" class="form-control" value="{{old('running_time')}}">
 
-                    <span class="form-text has-danger" v-if="form.errors.has('running_time')" v-text="form.errors.get('running_time')"></span>
+                    <span class="form-text has-danger"></span>
                 </div>
 
                 <div class="form-group">
-                    <label for="rating" class="label">Movie Rating</label>
-                    
-                    <input type="number" id="rating" name="rating" class="input" v-model="form.rating">
-    
-                    <span class="form-text has-danger" v-if="form.errors.has('rating')" v-text="form.errors.get('rating')"></span>
+                    <label for="rating" class="label">Movie Rating:</label>
+                                    
+                    <input type="text" id="rating" name="rating" class="form-control" value="{{old('rating')}}">
+                
+                    <span class="form-text has-danger"></span>
                 </div>
 
                 <div class="form-group">
-                    <label for="plot_synopsis" class="label">Movie Plot Synopsis</label>
-    
-                    <textarea id="plot_synopsis" name="plot_synopsis" class="input" v-model="form.plot_synopsis"></textarea>
-    
-                    <span class="form-text has-danger" v-if="form.errors.has('plot_synopsis')" v-text="form.errors.get('plot_synopsis')"></span>
+                    <label for="plot_synopsis" class="label">Movie Plot Synopsis:</label>
+                   
+                    {{ Form::textarea('plot_synopsis', null, ['class' => "form-control"]) }}
+
+                    <span class="form-text has-danger"></span>
                 </div>
 
-                <div class="form-group">
-                    <label for="director_id" class="label">Movie Director</label>
-                    <select v-model="form.director_id">
+                <div class="col-md-6 form-group">
+                    <label for="director_id" class="label">Movie Director:</label>
+                    <select name="director_id">
                         @foreach ($directors as $director)
                             <option value="{{ $director->id }}">{{ $director->first_name }} {{ $director->last_name }}</option>
                         @endforeach
                     </select>
+                    
+                    @if ($errors->has('director_id'))
+                    <span class="invalid-feedback">
+                        <strong>{{ $errors->first('director_id') }}</strong>
+                    </span>
+                    @endif
                 </div>
 
-                <div class="form-group">
-                    <label for="prod_comp_id" class="label">Movie Production Company</label>
-                    <select v-model="form.prod_comp_id">
-                        @foreach ($production_companies as $prod_company)
-                            <option value="{{ $prod_company->id }}">{{ $prod_company->name }}</option>
-                        @endforeach
-                    </select>
-                </div>
-
-                <div class="form-group">
-                    <label for="supplier_id" class="label">Movie Suppier</label>
-                    <select v-model="form.supplier_id">
+                <div class="col-md-6 form-group">
+                    <label for="supplier_id" class="label">Movie Supplier:</label>
+                    <select name="supplier_id">
                         @foreach ($suppliers as $supplier)
                             <option value="{{ $supplier->id }}">{{ $supplier->name }}</option>
                         @endforeach
                     </select>
+                    
+                    @if ($errors->has('supplier_id'))
+                    <span class="invalid-feedback">
+                        <strong>{{ $errors->first('supplier_id') }}</strong>
+                    </span>
+                    @endif
                 </div>
-            
-                <div class="form-group">
-                    <button class="btn btn-primary">Create</button>
+
+
+
+                <div class="col-md-6 form-group">
+                    <label for="prod_comp_id" class="label">Movie Production Company:</label>
+                    <select name="prod_comp_id">
+                        @foreach ($production_companies as $prod_comp)
+                            <option value="{{ $prod_comp->id }}">{{ $prod_comp->name }}</option>
+                        @endforeach
+                    </select>
+                    
+                    @if ($errors->has('prod_comp_id'))
+                    <span class="invalid-feedback">
+                        <strong>{{ $errors->first('prod_comp_id') }}</strong>
+                    </span>
+                    @endif
+                </div>
+
+                <div class="row">
+                    <div class="col-md-4"></div>
+                    <div class="form-group col-md-4" style="margin-top:60px">
+                        <button type="submit" class="btn btn-success" style="margin-left:38px">Create</button>
+                    </div>
                 </div>
             </form>
         </div>
