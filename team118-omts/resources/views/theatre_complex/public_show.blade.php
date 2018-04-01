2e50ee4aaccf27f5b9b9bd9e4846501f7036214d
Luis Rivera-Wong <lriverawong@outlook.com>
2018-04-01 17:19:39 -0400

    before undoing multi-form submission

--- team118-omts/resources/views/theatre_complex/public_show.blade.php
+++ team118-omts/resources/views/theatre_complex/public_show.blade.php
@@ -2,60 +2,81 @@
 
 @section('content')
     <div class="container">
-        @foreach ($show_times as $show_time)
-            <div id="show-time-form" class="container">
-                <form method="POST" action="{{URL('/shopping_cart')}}">
-                @csrf
-                    <div class="row">
-                        <div class="col-md-4"></div>
-                        <div class="form-group col-md-4">
-                        <label for="movie_id" class="label">Movie Id:</label>
-                        
-                        <input type="text" id="movie_id" name="movie_id" class="form-control" value="{{$show_time->movie_id}}"> 
-
-                        <span class="form-text has-danger"></span>
-                    </div>
-        
-                    <div class="form-group">
-                        <label for="theatre_id" class="label">Theatre Id:</label>
-                        
-                        <input type="text" id="theatre_id" name="theatre_id" class="form-control" value="{{$show_time->theatre_id}}">
-
-                        <span class="form-text has-danger"></span>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="theatre_complex_id" class="label">Theatre Complex Id:</label>
-                            
-                        <input type="text" id="theatre_complex_id" name="theatre_complex_id" class="form-control" value="{{$show_time->theatre_complex_id}}">
-        
-                        <span class="form-text has-danger"></span>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="showing_start_time" class="label">Showing Start Time:</label>
-                                
-                        <input type="text" id="showing_start_time" name="showing_start_time" class="form-control" value="{{$show_time->showing_start_time}}">
+        <h2 class="text-center">Current Showing at this Theatre Complex</h2>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col" class="text-center">Movie ID</th>
+                    <th scope="col" class="text-center">Theatre ID</th>
+                    <th scope="col" class="text-center">Theatre Complex ID</th>
+                    <th scope="col" class="text-center">Showing Start Time</th>
+                    <th scope="col" class="text-center">Number of Seats Available</th>
+                    <th scope="col" class="text-center">Desired Number of Seats</th>
+                </tr>
+            </thead>
             
-                        <span class="form-text has-danger"></span>
-                    </div>
+            <form method="POST" action="{{ route('cart.store') }}">
+            @csrf
+                <tbody>
+                    {{-- <div class="container">
+                        {{ $show_times->count() }}
+                        @foreach (range(0,$show_times->count()) as $x)
+                            <div class="container"
+                    </div> --}}
+                    @foreach ($show_times as $show_time)
+                        <tr class="single-buy">
+                            <div class="invisible">
+                                <input type="hidden" id="user_id-{{auth()->user()->id}}" name="user_id[]" class="form-control" value="{{auth()->user()->id}}" readonly> 
+                                <input type="hidden" id="showing_id-{{$show_time->id}}" name="showing_id[]" class="form-control" value="{{$show_time->id}}" readonly> 
+                            </div>
 
-                    <div class="form-group">
-                        <label for="num_seats_avail" class="label">Supplier Apartment Number:</label>
-                                    
-                        <input type="text" id="num_seats_avail" name="num_seats_avail" class="form-control" value="{{$show_time->num_seats_avail}}">
-                
-                        <span class="form-text has-danger"></span>
-                    </div>
-            
-                    <div class="row">
-                        <div class="col-md-4"></div>
-                        <div class="form-group col-md-4" style="margin-top:60px">
-                            <button type="submit" class="btn btn-success" style="margin-left:38px">Update</button>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        @endforeach
+                            <td class="form-group">
+                                {{-- <label for="movie_id" class="hide">Movie Id:</label> --}}
+                                {{-- <input type="hidden" id="movie_id" name="movie_id" class="form-control" value="{{$show_time->movie_id}}" readonly>  --}}
+                                <p class="text-center">{{$show_time->movie_id}}</p>
+                                <span class="form-text has-danger"></span>
+                            </td>    
+        
+                            <td class="form-group">
+                                {{-- <label for="theatre_id" class="label">Theatre Id:</label> --}}
+                                {{-- <input type="hidden" id="theatre_id" name="theatre_id" class="form-control" value="{{$show_time->theatre_id}}" readonly> --}}
+                                <p class="text-center">{{$show_time->theatre_id}}</p>
+                                <span class="form-text has-danger"></span>
+                            </td>
+    
+                            <td class="form-group">
+                                {{-- <label for="theatre_complex_id" class="label">Theatre Complex Id:</label> --}}
+                                {{-- <input type="hidden" id="theatre_complex_id" name="theatre_complex_id" class="form-control" value="{{$show_time->theatre_complex_id}}" readonly> --}}
+                                <p class="text-center">{{$show_time->theatre_complex_id}}</p>
+                                <span class="form-text has-danger"></span>
+                            </td>
+    
+                            <td class="form-group">
+                                {{-- <label for="showing_start_time" class="label">Showing Start Time:</label> --}}
+                                {{-- <input type="hidden" id="showing_start_time" name="showing_start_time" class="form-control" value="{{$show_time->showing_start_time}}" readonly> --}}
+                                <p class="text-center">{{$show_time->showing_start_time}}</p>
+                                <span class="form-text has-danger"></span>
+                            </td>
+
+                            <td class="form-group">
+                                {{-- <label for="showing_start_time" class="label">Showing Start Time:</label> --}}
+                                {{-- <input type="hidden" id="num_seats_avail" name="num_seats_avail" class="form-control" value="{{$show_time->num_seats_avail}}" readonly> --}}
+                                <p class="text-center">{{$show_time->num_seats_avail}}</p>
+                                <span class="form-text has-danger"></span>
+                            </td>
+
+                            <td class="form-group">
+                                {{-- <label for="showing_start_time" class="label">Showing Start Time:</label> --}}
+                                <input type="number" min="0" id="number_of_tickets" name="number_of_tickets[]" class="form-control" value="{{old('number_of_tickets')}}">
+                                <span class="form-text has-danger"></span>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+                <div class="float-right">
+                    <button type="submit" class="btn btn-success">Add to Shopping Cart</button>
+                </div>
+            </form>
+        </table>
     </div>
 @endsection()
