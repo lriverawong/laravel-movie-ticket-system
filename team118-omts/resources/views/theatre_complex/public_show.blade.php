dd2548862e20e4c5b08380118c1b7594698a2e19
Luis Rivera-Wong <lriverawong@outlook.com>
2018-04-01 17:21:30 -0400

    back to single form submission

--- team118-omts/resources/views/theatre_complex/public_show.blade.php
+++ team118-omts/resources/views/theatre_complex/public_show.blade.php
@@ -12,22 +12,17 @@
                     <th scope="col" class="text-center">Showing Start Time</th>
                     <th scope="col" class="text-center">Number of Seats Available</th>
                     <th scope="col" class="text-center">Desired Number of Seats</th>
+                    <th scope="col"></th>
                 </tr>
             </thead>
-            
-            <form method="POST" action="{{ route('cart.store') }}">
-            @csrf
-                <tbody>
-                    {{-- <div class="container">
-                        {{ $show_times->count() }}
-                        @foreach (range(0,$show_times->count()) as $x)
-                            <div class="container"
-                    </div> --}}
-                    @foreach ($show_times as $show_time)
-                        <tr class="single-buy">
+            <tbody>
+                @foreach ($show_times as $show_time)
+                    <tr class="single-buy">
+                        <form method="POST" action="{{ route('cart.store') }}">
+                        @csrf
                             <div class="invisible">
-                                <input type="hidden" id="user_id-{{auth()->user()->id}}" name="user_id[]" class="form-control" value="{{auth()->user()->id}}" readonly> 
-                                <input type="hidden" id="showing_id-{{$show_time->id}}" name="showing_id[]" class="form-control" value="{{$show_time->id}}" readonly> 
+                                <input type="hidden" id="user_id" name="user_id" class="form-control" value="{{auth()->user()->id}}" readonly> 
+                                <input type="hidden" id="showing_id" name="showing_id" class="form-control" value="{{$show_time->id}}" readonly> 
                             </div>
 
                             <td class="form-group">
@@ -52,6 +47,7 @@
                             </td>
     
                             <td class="form-group">
+
                                 {{-- <label for="showing_start_time" class="label">Showing Start Time:</label> --}}
                                 {{-- <input type="hidden" id="showing_start_time" name="showing_start_time" class="form-control" value="{{$show_time->showing_start_time}}" readonly> --}}
                                 <p class="text-center">{{$show_time->showing_start_time}}</p>
@@ -59,24 +55,25 @@
                             </td>
 
                             <td class="form-group">
-                                {{-- <label for="showing_start_time" class="label">Showing Start Time:</label> --}}
-                                {{-- <input type="hidden" id="num_seats_avail" name="num_seats_avail" class="form-control" value="{{$show_time->num_seats_avail}}" readonly> --}}
-                                <p class="text-center">{{$show_time->num_seats_avail}}</p>
-                                <span class="form-text has-danger"></span>
-                            </td>
+                                    {{-- <label for="showing_start_time" class="label">Showing Start Time:</label> --}}
+                                    {{-- <input type="hidden" id="num_seats_avail" name="num_seats_avail" class="form-control" value="{{$show_time->num_seats_avail}}" readonly> --}}
+                                    <p class="text-center">{{$show_time->num_seats_avail}}</p>
+                                    <span class="form-text has-danger"></span>
+                                </td>
 
                             <td class="form-group">
                                 {{-- <label for="showing_start_time" class="label">Showing Start Time:</label> --}}
-                                <input type="number" min="0" id="number_of_tickets" name="number_of_tickets[]" class="form-control" value="{{old('number_of_tickets')}}">
+                                <input type="number" min="0" id="number_of_tickets" name="number_of_tickets" class="form-control" value="{{old('number_of_tickets')}}">
                                 <span class="form-text has-danger"></span>
                             </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-                <div class="float-right">
-                    <button type="submit" class="btn btn-success">Add to Shopping Cart</button>
-                </div>
-            </form>
+        
+                            <td class="form-group col-md-4" style="margin-top:60px">
+                                <button type="submit" class="btn btn-success" style="margin-left:38px">Add to Shopping Cart</button>
+                            </td>
+                        </form>
+                    </tr>
+                @endforeach
+            </tbody>
         </table>
     </div>
 @endsection()
