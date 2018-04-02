2a33ca49e9fa1d2aa1b62fc834ca0d49167590cb
Luis Rivera-Wong <lriverawong@outlook.com>
2018-04-02 13:14:10 -0400

    before pres

--- team118-omts/resources/views/public_home.blade.php
+++ team118-omts/resources/views/public_home.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="container">
         <div class="jumbotron">
-            <h1 class="display-3 text-center">Welcome to the Team 118 - Onlive Movie Ticket Service!</h1>
+            <h1 class="display-3 text-center">Welcome to the Team 118 - Online Movie Ticket Service!</h1>
             <p class="lead text-center">
                 Feel free to look around although to best enjoy our site - Sign up!
             </p>
